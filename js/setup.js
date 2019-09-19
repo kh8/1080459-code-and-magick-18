1d06bdbeec20ac0bc3f739f5e1735dd5e8721992
kh8 <mihailk1@ya.ru>
2019-09-19 17:08:46 +0500

    attempt5

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var WIZARDS_COUNT = 4;
+var wizardData = {
+  NAMES: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
+  SURNAMES: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
+  COAT_COLORS: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
+  EYES_COLORS: ['black', 'red', 'blue', 'yellow', 'green']
+};
+
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
@@ -8,26 +16,8 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
-var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-var wizards = [];
-
 var generateWizard = function () {
-  var generateWizardName = function () {
-    return (Math.random() >= 0.5) ? WIZARD_NAMES[Math.floor(Math.random() * WIZARD_NAMES.length)].concat(' ' + WIZARD_SURNAMES[Math.floor(Math.random() * WIZARD_SURNAMES.length)]) : WIZARD_SURNAMES[Math.floor(Math.random() * WIZARD_SURNAMES.length)].concat(' ' + WIZARD_NAMES[Math.floor(Math.random() * WIZARD_NAMES.length)]);
-  };
-
-  var generateWizardCoat = function () {
-    return WIZARD_COAT_COLORS[Math.floor((Math.random() * WIZARD_COAT_COLORS.length))];
-  };
-
-  var generateWizardEyes = function () {
-    return WIZARD_EYES_COLORS[Math.floor((Math.random() * WIZARD_EYES_COLORS.length))];
-  };
-
-  return {name: generateWizardName(), coatColor: generateWizardCoat(), eyesColor: generateWizardEyes()};
+  return {name: (Math.random() >= 0.5) ? wizardData.NAMES[Math.floor(Math.random() * wizardData.NAMES.length)].concat(' ' + wizardData.SURNAMES[Math.floor(Math.random() * wizardData.SURNAMES.length)]) : wizardData.SURNAMES[Math.floor(Math.random() * wizardData.SURNAMES.length)].concat(' ' + wizardData.NAMES[Math.floor(Math.random() * wizardData.NAMES.length)]), coatColor: wizardData.COAT_COLORS[Math.floor((Math.random() * wizardData.COAT_COLORS.length))], eyesColor: wizardData.EYES_COLORS[Math.floor((Math.random() * wizardData.EYES_COLORS.length))]};
 };
 
 
@@ -40,11 +30,15 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < 4; i++) {
-  wizards[i] = generateWizard();
-  fragment.appendChild(renderWizard(wizards[i]));
-}
+var renderFragment = function (fragment) {
+  var wizards = [];
+  for (var i = 0; i < WIZARDS_COUNT; i++) {
+    wizards[i] = generateWizard();
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+};
 
+var fragment = document.createDocumentFragment();
+renderFragment(fragment);
 similarListElement.appendChild(fragment);
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
